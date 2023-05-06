98cfe0cddb7c2668e9ef86176f4927cde8cd8c96
Abner Lima <abnerlima.1997@gmail.com>
2023-05-06 17:44:02 -0300

    Listando carros GET

--- Carangas/Rest.swift
+++ Carangas/Rest.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+enum CarError {
+    case url
+    case taskError(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case invalidJson
+}
+
 class REST {
     private static let basePath = "https://carangas.herokuapp.com/cars"
     
@@ -23,5 +32,34 @@ class REST {
     
     private static let session = URLSession(configuration: configuration)
     
-    
+    class func loadCars(onComplete: @escaping ([Car]) -> Void, onError: @escaping (CarError) -> Void) {
+        guard let url = URL(string: basePath) else {
+            onError(.url)
+            return
+        }
+        
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse else {
+                    onError(.noResponse)
+                    return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data else { return }
+                    do {
+                        let cars = try JSONDecoder().decode([Car].self, from: data)
+                        onComplete(cars)
+                    } catch {
+                        onError(.invalidJson)
+                    }
+                } else {
+                    onError(.responseStatusCode(code: response.statusCode))
+                }
+            } else {
+                onError(.taskError(error: error!))
+            }
+        }
+        
+        dataTask.resume()
+    }
 }
