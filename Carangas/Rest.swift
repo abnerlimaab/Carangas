71f781e1ac8f4e69cbbf8e2b4c09b3b7cbdcca1f
Abner Lima <abnerlima.1997@gmail.com>
2023-05-06 18:46:20 -0300

    Adicionando carros POST

--- Carangas/Rest.swift
+++ Carangas/Rest.swift
@@ -31,7 +31,7 @@ class REST {
     }()
     
     private static let session = URLSession(configuration: configuration)
-    
+  
     class func loadCars(onComplete: @escaping ([Car]) -> Void, onError: @escaping (CarError) -> Void) {
         guard let url = URL(string: basePath) else {
             onError(.url)
@@ -57,6 +57,35 @@ class REST {
                 }
             } else {
                 onError(.taskError(error: error!))
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+    class func saveCar(car: Car, onComplete: @escaping (Bool) -> Void) {
+        guard let url = URL(string: basePath) else {
+            onComplete(false)
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        guard let json = try? JSONEncoder().encode(car) else {
+            onComplete(false)
+            return
+        }
+        request.httpBody = json
+        
+        let dataTask = session.dataTask(with: request) { data, respone, error in
+            if error == nil {
+                guard let response = respone as? HTTPURLResponse, response.statusCode == 200, let _ = data else {
+                    onComplete(false)
+                    return
+                }
+                onComplete(true)
+            } else {
+                onComplete(false)
             }
         }
         
