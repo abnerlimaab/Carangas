86f392567ce84b63cd92fc09da09e6a191b0b5be
Abner Lima <abnerlima.1997@gmail.com>
2023-05-07 10:43:39 -0300

    Utilizando webview

--- Carangas/CarViewController.swift
+++ Carangas/CarViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import WebKit
 
 class CarViewController: UIViewController {
     
@@ -17,7 +18,9 @@ class CarViewController: UIViewController {
     @IBOutlet weak var lbBrand: UILabel!
     @IBOutlet weak var lbGasType: UILabel!
     @IBOutlet weak var lbPrice: UILabel!
-
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
     // MARK: - Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +28,39 @@ class CarViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        title = car.name
-        lbBrand.text = car.brand
-        lbGasType.text = car.gas
-        lbPrice.text = "R$ \(car.price)"
+        prepareCarInfo()
+        prepareWebView()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination as! AddEditViewController
         vc.car = car
     }
+    
+    func prepareCarInfo() {
+        title = car.name
+        lbBrand.text = car.brand
+        lbGasType.text = car.gas
+        lbPrice.text = "R$ \(car.price)"
+    }
+    
+    func prepareWebView() {
+        let name = "\(car.name)+\(car.brand.replacingOccurrences(of: " ", with: "+"))"
+        let urlString = "https://www.google.com.br/search?q=\(name)&tbm=isch"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        webView.allowsBackForwardNavigationGestures = true
+        webView.allowsLinkPreview = true
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.load(request)
+    }
 
+}
+
+extension CarViewController: WKNavigationDelegate, WKUIDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loading.stopAnimating()
+    }
 }
